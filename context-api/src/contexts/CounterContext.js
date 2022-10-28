5da7709b96d02c71284e52c94ee48932f9bc87b6
S550Stang <prabhuvardhanrao94@gmail.com>
2022-10-28 07:14:37 -0400

    Feature Cleanup

--- context-api/src/contexts/CounterContext.js
+++ context-api/src/contexts/CounterContext.js
@@ -1,26 +1,24 @@
-import React,{useState,createContext} from 'react'
-
+import React, { useState, createContext } from "react";
 
 export const CounterContextApi = createContext();
 
 export const CounterContextProvider = (props) => {
-
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   const incrementFunction = () => {
-    setCount(count+1)
-  }
+    setCount(count + 1);
+  };
   const decrementFunction = () => {
-    setCount(count-1)
-  }
+    setCount(count - 1);
+  };
 
-  const value = {count,incrementFunction,decrementFunction}
+  const value = { count, incrementFunction, decrementFunction };
 
   return (
     <CounterContextApi.Provider value={value}>
       {props.children}
     </CounterContextApi.Provider>
-  )
-}
+  );
+};
 
 export default CounterContextProvider;
